e1798e0cb866eba8d177bf47360eb9c3235c885f
Justin Fu <justinjfu@gmail.com>
2017-07-21 15:28:10 -0700

    add gail example

---
+++ scripts/pendulum_gail.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+from sandbox.rocky.tf.policies.gaussian_mlp_policy import GaussianMLPPolicy
+from sandbox.rocky.tf.envs.base import TfEnv
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.gym_env import GymEnv
+
+
+from inverse_rl.algos.irl_trpo import IRLTRPO
+from inverse_rl.models.imitation_learning import GAIL
+from inverse_rl.utils.log_utils import rllab_logdir, load_latest_experts
+
+def main():
+    env = TfEnv(GymEnv('Pendulum-v0', record_video=False, record_log=False))
+    
+    experts = load_latest_experts('data/pendulum', n=5)
+
+    irl_model = GAIL(env_spec=env.spec, expert_trajs=experts)
+    policy = GaussianMLPPolicy(name='policy', env_spec=env.spec, hidden_sizes=(32, 32))
+    algo = IRLTRPO(
+        env=env,
+        policy=policy,
+        irl_model=irl_model,
+        n_itr=200,
+        batch_size=1000,
+        max_path_length=100,
+        discount=0.99,
+        store_paths=True,
+        discrim_train_itrs=50,
+        irl_model_wt=1.0,
+        entropy_weight=0.0, # GAIL should not use entropy unless for exploration
+        zero_environment_reward=True,
+        baseline=LinearFeatureBaseline(env_spec=env.spec)
+    )
+
+    with rllab_logdir(algo=algo, dirname='data/pendulum_gail'):
+        with tf.Session():
+            algo.train()
+
+if __name__ == "__main__":
+    main()
